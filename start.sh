6392a48da157f2606fd95f7cf7c4b7523380b4f0
YiJie <yijie4188@gmail.com>
2026-02-23 22:36:53 +0800

    style: standardize string quotes in start.sh script
    Use single quotes consistently for string literals in shell script to follow best practices

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ export HOME=`realpath ./.ai/.mock`
 
 export __VF_PROJECT_WORKSPACE_FOLDER__=`realpath .`
 export __VF_PROJECT_AI_ADAPTER_CLAUDE_CODE_CLI_PATH__='./packages/adapters/claude-code/node_modules/.bin/ccr'
-export __VF_PROJECT_AI_ADAPTER_CLAUDE_CODE_CLI_ARGS__="code"
+export __VF_PROJECT_AI_ADAPTER_CLAUDE_CODE_CLI_ARGS__='code'
 
 # 后台运行 server
 npx vfui-server | tee .logs/server.log &
@@ -10,6 +10,6 @@ npx vfui-server | tee .logs/server.log &
 npx vfui-client | tee .logs/client.log &
 
 # 退出时终止所有后台进程
-trap "kill 0" EXIT
+trap 'kill 0' EXIT
 # 等待所有后台进程结束
 wait
