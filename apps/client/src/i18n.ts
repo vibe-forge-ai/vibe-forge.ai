33150758853ec2f12b358db4d2fada243d436768
YiJie <yijie4188@gmail.com>
2026-04-18 02:11:30 +0800

    feat(client): hot reload locale resources

--- apps/client/src/i18n.ts
+++ apps/client/src/i18n.ts
@@ -2,21 +2,39 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-import en from './resources/locales/en.json'
-import zh from './resources/locales/zh.json'
+import {
+  applyHotTranslationUpdates,
+  buildTranslationResources
+} from './i18n-resources'
+import type { LocaleModule, LocaleModuleMap } from './i18n-resources'
+
+const localeModules = import.meta.glob('./resources/locales/*.json', {
+  eager: true
+}) as LocaleModuleMap
+const localeModulePaths = Object.keys(localeModules).sort()
 
 void i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      zh: { translation: zh },
-      en: { translation: en }
-    },
+    resources: buildTranslationResources(localeModules),
     fallbackLng: 'zh',
     interpolation: {
       escapeValue: false
+    },
+    react: {
+      bindI18nStore: 'added'
     }
   })
+
+if (import.meta.hot) {
+  import.meta.hot.accept(localeModulePaths, (nextModules) => {
+    applyHotTranslationUpdates({
+      instance: i18n,
+      modulePaths: localeModulePaths,
+      nextModules: nextModules as Array<LocaleModule | undefined>
+    })
+  })
+}
 
 export default i18n
